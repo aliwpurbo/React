32b8f258e944c80aff47afd1b443d6fc2067ca63
aliwpurbo <ali.wpurbo@gmail.com>
2020-11-25 13:09:13 +0900

    sampai dengan pagination

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import JumbotronComponent from "./components/JumbotronComponent";
+import NavbarComponent from "./components/NavbarComponent";
+import TableComponent from "./components/TableComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    title: "Data Siswa",
+    users: [
+      {
+        id: 1,
+        nama: "Ali",
+        alamat: "Jl Leo Mamiri",
+      },
+
+      {
+        id: 2,
+        nama: "Adi",
+        alamat: "Jl Leo Mamiri",
+      },
+
+      {
+        id: 3,
+        nama: "Ami",
+        alamat: "Jl Leo Mamiri",
+      },
+      {
+        id: 4,
+        nama: "Aji",
+        alamat: "Jl Leo Mamiri",
+      },
+      {
+        id: 4,
+        nama: "Ami",
+        alamat: "Jl Leo Mamiri",
+      },
+      {
+        id: 5,
+        nama: "Ami",
+        alamat: "Jl Leo Mamiri",
+      },
+      {
+        id: 6,
+        nama: "Ami",
+        alamat: "Jl Leo Mamiri",
+      },
+      {
+        id: 7,
+        nama: "Madun",
+        alamat: "Jl Leo Mamiri",
+      },
+      {
+        id: 8,
+        nama: "Sri",
+        alamat: "Jl Leo Mamiri",
+      },
+      {
+        id: 9,
+        nama: "Ajis",
+        alamat: "Jl Leo Mamiri",
+      },
+      {
+        id: 10,
+        nama: "Amsi",
+        alamat: "Jl Leo Mamiri",
+      },
+      {
+        id: 11,
+        nama: "Amis",
+        alamat: "Jl Leo Mamiri",
+      },
+      {
+        id: 12,
+        nama: "Amid",
+        alamat: "Jl Leo Mamiri",
+      },
+    ],
+  };
+  render() {
+    return (
+      <div>
+        <NavbarComponent />
+        <JumbotronComponent title={this.state.title} />
+        <TableComponent users={this.state.users} />
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/components/TableComponent.js
@@ -0,0 +1,84 @@
+import BootstrapTable from "react-bootstrap-table-next";
+import React from "react";
+import { Container, Button } from "reactstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfo, faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
+import paginationFactory from 'react-bootstrap-table2-paginator';
+
+const { SearchBar } = Search;
+
+const defaultSorted = [
+  {
+    dataField: "id",
+    order: "asc",
+  },
+];
+
+const TableComponent = (props) => {
+  const columns = [
+    {
+      dataField: "id",
+      text: "ID",
+      sort: true,
+      headerStyle: () => {
+        return { width: "5%" };
+      },
+    },
+    {
+      dataField: "nama",
+      text: "Nama",
+      sort: true,
+    },
+    {
+      dataField: "alamat",
+      text: "Alamat",
+      sort: true,
+    },
+    {
+      dataField: "link",
+      text: "Action",
+      formatter: (rowContent, row) => {
+        return (
+          <div>
+            <Button color="dark" className="mr-2">
+              <FontAwesomeIcon icon={faInfo} /> Detail
+            </Button>
+
+            <Button color="dark" className="mr-2">
+              <FontAwesomeIcon icon={faEdit} /> Edit
+            </Button>
+
+            <Button color="dark" className="mr-2">
+              <FontAwesomeIcon icon={faTrash} /> Delete
+            </Button>
+          </div>
+        );
+      },
+    },
+  ];
+
+  return (
+    <Container>
+      <ToolkitProvider
+        bootstrap4
+        keyField="id"
+        data={props.users}
+        columns={columns}
+        defaultSorted={defaultSorted}
+        search
+      >
+        {(props) => (
+          <div>
+            <div className="float-right">
+              <SearchBar {...props.searchProps} placeholder="Search ..." />
+            </div>
+            <BootstrapTable {...props.baseProps} pagination={ paginationFactory() } />
+          </div>
+        )}
+      </ToolkitProvider>
+    </Container>
+  );
+};
+
+export default TableComponent;
